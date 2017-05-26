2bafade257a46502ead0112d790a72e6b3bc5ac8
Kipka <jan.kipka@hpe.com>
2017-05-26 17:05:17 +0200

    Frontend für Submit created, Backend auf eigenem Branch

--- src/pages/submit-quote/submit-quote.ts
+++ src/pages/submit-quote/submit-quote.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import firebase from 'firebase';
 
 /**
  * Generated class for the SubmitQuote page.
@@ -14,11 +15,74 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SubmitQuote {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  authorData: any = [];
+
+  trueOrFalse: any = "";
+
+  category: number = 0;
+
+  author: any = "";
+
+  quote: any = "";
+
+  source: any = "";
+
+  date: any = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertController: AlertController) {
+  }
+
+  ionViewWillEnter(){
+    firebase.database().ref('Quoteclub/authors/').once('value', snapshot => {
+      for( let i in snapshot.val()){
+        let tempArray = {
+          id: i,
+          name: snapshot.val()[i]
+        }
+        this.authorData.push(tempArray);
+      }
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SubmitQuote');
+  }
+
+  catchEvent() {
+
+    if(!this.checkInputs()){
+
+    }else{
+      this.showAlert("Nicht alle Felder wurden ausgefüllt. Alle Angaben sind erforderlich.")
+    }
+
+  }
+
+  checkInputs() {
+    if (this.trueOrFalse == "" || this.category == 0 || this.author == ""
+      || this.quote == "" || this.source == "" || this.date == "") {
+        return true;
+    }
+    return false;
+  }
+
+  showAlert(message: any){
+    let alert = this.alertController.create({
+      title: 'Fehlende Angaben',
+      message: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  submitQuote(){
+
+    //IN ARBEIT
+
+    return firebase.database().ref('Quoteclub/quotes/').set({
+      category: this.category,
+      quote: this.quote
+    })
   }
 
 }

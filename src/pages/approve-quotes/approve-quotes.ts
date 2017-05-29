2282e4d65dd6a335638401992cf97b7fd1e978dc
xqtion <andre-h96@gmx.de>
2017-05-29 20:13:30 +0200

    First IFs reworked

--- src/pages/approve-quotes/approve-quotes.ts
+++ src/pages/approve-quotes/approve-quotes.ts
@@ -22,8 +22,12 @@ export class ApproveQuotes {
   }
 
   editStatus: string = "submitted";
+  dataQuotes: any;
   dataSubmitted: any;
   dataReported: any;
+  dataUser: any;
+  dataAuthor: any;
+  dataCategory: any;
   loading: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private quoteManagementProvider: QuoteManagementProvider, private alertCtrl: AlertController, private loadingCtrl: LoadingController) {
@@ -37,6 +41,17 @@ export class ApproveQuotes {
     if (showLoading) {
       this.createAndShowLoading();
     }
+    this.quoteManagementProvider.setQuotes().then((data) => {
+      this.dataQuotes = this.quoteManagementProvider.dataQuotes;
+      console.log(this.quoteManagementProvider.dataQuotes);
+      if(event!=null){
+        event.complete();
+      }
+    }).catch(function (error) {
+      if (showLoading) {
+        this.createAndShowErrorAlert(error);
+      }
+    });
     this.quoteManagementProvider.setSubmitted().then((data) => {
       this.dataSubmitted = this.quoteManagementProvider.dataSubmittedQuotes;
       console.log(this.quoteManagementProvider.dataSubmittedQuotes);
@@ -51,6 +66,40 @@ export class ApproveQuotes {
     this.quoteManagementProvider.setReported().then((data) => {
       this.dataReported = this.quoteManagementProvider.dataReportedQuotes;
       console.log(this.quoteManagementProvider.dataReportedQuotes);
+      if(event!=null){
+        event.complete();
+      }
+    }).catch(function (error) {
+      if (showLoading) {
+        this.createAndShowErrorAlert(error);
+      }
+    });
+    this.quoteManagementProvider.setCategory().then((data) => {
+      this.dataCategory = this.quoteManagementProvider.dataCategory;
+      console.log(this.quoteManagementProvider.dataCategory);
+      if(event!=null){
+        event.complete();
+      }
+    }).catch(function (error) {
+      if (showLoading) {
+        this.createAndShowErrorAlert(error);
+      }
+    });
+    this.quoteManagementProvider.setAuthor().then((data) => {
+      this.dataAuthor = this.quoteManagementProvider.dataAuthor;
+      console.log("AUTHORS");
+      console.log(this.quoteManagementProvider.dataAuthor);
+      if(event!=null){
+        event.complete();
+      }
+    }).catch(function (error) {
+      if (showLoading) {
+        this.createAndShowErrorAlert(error);
+      }
+    });
+    this.quoteManagementProvider.setUser().then((data) => {
+      this.dataUser = this.quoteManagementProvider.dataUser;
+      console.log(this.quoteManagementProvider.dataUser);
       if (showLoading) {
       this.loading.dismiss().catch((error) => console.log(error));
       }
@@ -80,8 +129,33 @@ export class ApproveQuotes {
     this.loading.present();
   }
 
-  openDetails(event, value){
-    this.navCtrl.push(QuoteDetail, { quoteItem: value });
+  openDetails(event, submittedItem, reportedItem){
+    this.navCtrl.push(QuoteDetail, { submittedItem: submittedItem, reportedItem: reportedItem, dataCategory: this.dataCategory, dataUser: this.dataUser, dataAuthor: this.dataAuthor, dataQuotes: this.dataQuotes});
   }
 
+  delete(item){
+     let confirm = this.alertCtrl.create({
+      title: 'Wirklich löschen?',
+      message: 'Der Löschvorgang kann nicht mehr rückgängig gemacht werden',
+      buttons: [
+        {
+          text: 'Nein',
+          handler: () => {}
+        },
+        {
+          text: 'Ja',
+          handler: () => {
+            item.deleted = true;
+            //firebase delete
+            //evtl. Meldung = nicht approved bzw. Report nicht aussagekräftig genug
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  doRefresh(refresher) {
+    this.loadData(false, refresher);
+  }
 }
--- src/providers/quoteManagement-provider.ts
+++ src/providers/quoteManagement-provider.ts
@@ -1,16 +1,33 @@
 import firebase from 'firebase';
 import { Injectable } from '@angular/core';
-//import * as _ from 'lodash';
+import * as _ from 'lodash';
 
 @Injectable()
 export class QuoteManagementProvider {
 
+    dataQuotes: Array<any>;
     dataSubmittedQuotes: Array<any>;
     dataReportedQuotes: Array<any>;
+    dataUser: Array<any>;
+    dataCategory: Array<any>;
+    dataAuthor: Array<any>;
 
 
     constructor() {
 
+    }
+
+    setQuotes() {
+        return firebase.database().ref('quotes').once('value', snapshot => {
+            let quotesArray = [];
+            let counter = 0;
+            for (let i in snapshot.val()) {
+                quotesArray[counter] = snapshot.val()[i];
+                quotesArray[counter].id = i;
+                counter++;
+            }
+            this.dataQuotes = quotesArray;
+        });
     }
 
     setSubmitted() {
@@ -20,10 +37,10 @@ export class QuoteManagementProvider {
             for (let i in snapshot.val()) {
                 submittedArray[counter] = snapshot.val()[i];
                 submittedArray[counter].id = i;
+                submittedArray[counter].deleted = false;
                 counter++;
             }
             this.dataSubmittedQuotes = submittedArray;
-            //this.dataUser = _.sortBy(this.dataUser, "score").reverse();
         });
     }
 
@@ -34,10 +51,50 @@ export class QuoteManagementProvider {
             for (let i in snapshot.val()) {
                 reportedArray[counter] = snapshot.val()[i];
                 reportedArray[counter].id = i;
+                reportedArray[counter].deleted = false;
                 counter++;
             }
             this.dataReportedQuotes = reportedArray;
-            //this.dataUser = _.sortBy(this.dataUser, "score").reverse();
+        });
+    }
+    
+    setUser() {
+        return firebase.database().ref('user').once('value', snapshot => {
+            let userArray = [];
+            let counter = 0;
+            for (let i in snapshot.val()) {
+                userArray[counter] = snapshot.val()[i];
+                userArray[counter].id = i;
+                counter++;
+            }
+            this.dataUser = userArray;
+            this.dataUser = _.sortBy(this.dataUser, "score").reverse();
+        });
+    }
+
+    setAuthor() {
+        return firebase.database().ref('authors').once('value', snapshot => {
+            let authorArray = [];
+            let counter = 0;
+            for (let i in snapshot.val()) {
+                authorArray[counter] = snapshot.val()[i];
+                authorArray[counter].id = i;
+                counter++;
+            }
+            this.dataAuthor = authorArray;
+        });
+    }
+
+    setCategory() {
+        return firebase.database().ref('category').once('value', snapshot => {
+            let categoryArray = [];
+            let counter = 0;
+            for (let i in snapshot.val()) {
+                categoryArray[counter] = snapshot.val()[i];
+                categoryArray[counter].id = i;
+                counter++;
+            }
+            this.dataCategory = categoryArray;
         });
     }
 
